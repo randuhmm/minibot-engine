46cae47571340f94a4ed4a787d00b9b097f2c6ff
Jonny Morrill <jrmorrill@gmail.com>
2013-12-26 05:06:08 +0000

    adding polyfill to requestAnimationFrame
    git-svn-id: svn://localhost/tmp/testgc/trunk@46 b6c228b4-7f25-468a-b3ab-662a24036a6f

--- src/minibot/system/web.js
+++ src/minibot/system/web.js
@@ -8,6 +8,31 @@ define(
 	)
 	{
 
+		(function() {
+			var lastTime = 0;
+			var vendors = ['webkit', 'moz'];
+			for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+				window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+				window.cancelAnimationFrame =
+					window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+			}
+
+			if (!window.requestAnimationFrame)
+				window.requestAnimationFrame = function(callback, element) {
+					var currTime = new Date().getTime();
+					var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+					var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+					  timeToCall);
+					lastTime = currTime + timeToCall;
+					return id;
+				};
+
+			if (!window.cancelAnimationFrame)
+				window.cancelAnimationFrame = function(id) {
+					clearTimeout(id);
+				};
+		}());
+
 		function decorate(object, traits) {
 			for (var accessor in traits) {
 				object[accessor]= traits[accessor];
@@ -72,6 +97,12 @@ define(
 		{
 			var scene = new CanvasScene(options);
 			return scene;
+		};
+
+		// 
+		system.GetPlatformType = function()
+		{
+
 		};
 		
 		// <-- Public System Methods
