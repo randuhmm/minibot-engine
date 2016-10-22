7e4d114e20c0452af26b9055e6282a5ee01b70d4
Jonny Morrill <jrmorrill@gmail.com>
2016-10-21 17:39:55 -0700

    Mask

--- src/minibot/display/scene/Mask.js
+++ src/minibot/display/scene/Mask.js
@@ -2,41 +2,41 @@ define(
 	['minibot/display/canvas/CanvasDisplayObject'],
 	function(CanvasDisplayObject)
 	{
-		return Class.create(
+		var Mask = Class.create(
 			CanvasDisplayObject,
 			{
-				
+
 				base: null,
 				mask: null,
-				
+
 				maskCanvas: null,
 				maskContext: null,
-				
+
 				initialize: function($super, base, mask)
 				{
 					$super();
-					
+
 					this.base = base;
 					this.mask = mask;
-					
+
 					// Setup the mask canvas
 					this.maskCanvas = new Element('canvas');
 					this.maskCanvas.width = this.base.w + this.base.x;
 					this.maskCanvas.height = this.base.h + this.base.y;
 					this.maskContext = this.maskCanvas.getContext('2d');
-					
+
 					// Draw the mask pixels to the canvas
 					this.mask.render(0, this.maskContext, this.base.x, this.base.y);
-					
+
 					this.maskContext.globalCompositeOperation = 'source-in';
-					
+
 					this.base.render(0, this.maskContext, 0, 0);
-					
+
 					this.w = this.maskCanvas.width;
 					this.h = this.maskCanvas.height;
-					
+
 				},
-				
+
 				render: function(dt, context, x, y)
 				{
 					context.drawImage(
@@ -51,8 +51,9 @@ define(
 						this.h //dh
 					)
 				}
-				
+
 			}
 		);
+		return Mask;
 	}
 );
